265334d050b53bac7ad9e61a512f9f22fe2b9977
jcleberlo <jcleberlo@hotmail.com>
2023-11-08 20:24:53 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+class heroi {
+    constructor (nome, idade, tipo) {
+      this.nome = nome
+      this.idade = idade
+      this.tipo = tipo
+    }
+  
+    atacar () {
+      let ataque = ""
+      switch (this.tipo) {
+        case "mago":
+          ataque = "magia"
+          break
+        case "guerreiro":
+          ataque = "espada"
+          break
+        case "monge":
+          ataque = "artes marciais"
+          break
+        case "ninja":
+          ataque = "shuriken"
+          break
+      }
+  
+      console.log (`O ${this.tipo} atacou usando ${ataque}.`);
+    }
+  }
+
+let heroiMago = new heroi ("Antonio" , 15 , "mago" , "magia")
+let heroiGuerreiro = new heroi ("José" , 16 , "guerreiro" , "espada")
+let heroiMonge = new heroi ("Francisco" , 17 , "monge", "artes marciais")
+let heroiNinja = new heroi ("Raimundo" , 14 , "ninja" , "shuriken")
+
+heroiMago.atacar ()
+heroiGuerreiro.atacar ()
+heroiMonge.atacar ()
+heroiNinja.atacar ()
